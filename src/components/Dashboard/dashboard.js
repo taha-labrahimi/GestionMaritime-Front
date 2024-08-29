0ace938be203f0b34cea0b3863e6e778d7b9ee0e
TAHA LABRAHIMI <144866467+taha-labrahimi@users.noreply.github.com>
2024-08-29 17:11:22 +0100

    timer

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -1,10 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'; // Use `useNavigate` instead of `useHistory`
 import Sidebar from '../Sidebar/Sidebar';
 import Welcome from '../Dashboard/Welcome';
 import './Dashboard.css';
 
 function Dashboard() {
     const [content, setContent] = useState(<Welcome />);
+    const navigate = useNavigate(); 
+
+    useEffect(() => {
+        const sessionTimeout = setTimeout(() => {
+            localStorage.removeItem('token');
+            alert('Your session has ended. You will be redirected to the login page.');
+            navigate('/'); 
+        }, 3600000); 
+
+        return () => clearTimeout(sessionTimeout);
+    }, [navigate]); 
 
     const handleContentChange = (component) => {
         setContent(component);
